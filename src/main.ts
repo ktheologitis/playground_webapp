0aae7482ec8b74dadfbe6390974b0bcccd419348
Konstantinos Theologitis <ktheologitis@gmail.com>
2025-07-05 23:14:32 +0200

    chore: test pre-commit again

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
-const foo: any = 123;
-const f = "3";
+// const foo: any = 123;
+// const f = "3";
+console.log("dummy text");
 
 createApp(App).mount("#app");
