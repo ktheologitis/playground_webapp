16ade9bc757037c774785a4af0210fef242be28d
Konstantinos Theologitis <ktheologitis@gmail.com>
2025-07-06 12:19:48 +0200

    chore: commit 2

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
+console.log("dummy change"); // This is a dummy change to trigger a commit
 createApp(App).mount("#app");
