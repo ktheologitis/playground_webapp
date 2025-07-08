0d49038aa210a52270d9a46ebd657e02e75202ee
Konstantinos Theologitis <ktheologitis@gmail.com>
2025-07-08 18:09:32 +0200

    feat: test git merge

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
+console.log("change commited from branch: 'feat/test-git-merge'");
 createApp(App).mount("#app");
